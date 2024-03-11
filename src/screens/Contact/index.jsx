fa220c9dd6b907a183d15848a2baa57ec71bbeae
Matheszambondacosta <matheus-costa@aluno.senai.br>
2024-03-11 14:43:24 -0300

    Atualizações nos estilos e nomes das telas

--- src/screens/Contact/index.jsx
+++ src/screens/Contact/index.jsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function index() {
+export default function contact() {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Text>Contact</Text>
     </View>
   )
 }
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function index() {
+export default function Home() {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Text>Home</Text>
     </View>
   )
-}
+} 
