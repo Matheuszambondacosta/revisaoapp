7e8ed7b0889b9812c795b3fed04c89c7ace41b9d
Matheszambondacosta <matheus-costa@aluno.senai.br>
2024-03-11 14:39:21 -0300

    Adiciona componentes Title, Contact e Home e estilos para o componente Title

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import styles from './styles'
+
+const Title = ({ title }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{Title}</Text>
+    </View>
+  )
+}
+
+export default Title
---
+++ src/components/Title/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+
+  },
+});
+
+export default styles;
